44324ed2f9bffde92072d4a58cef16d2f138c331
Zouhair.ETTARAK <ettarak.zouhair@gmail.com>
2023-03-21 17:29:16 +0000

    add foreignId to table Posts

---
+++ database/migrations/2023_03_21_172340_add_foreign_keys_to_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('categoy_id')->constrained('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['categoy_id']);
+
+            $table->dropColumn('categoy_id');
+            $table->dropColumn('user_id');
+        });
+    }
+};
